6bcc9a503d84d247bdb29a20e976af04c7ed0acf
chandan-naik-ds <chandannnaik03@gmail.com>
2026-02-27 09:31:54 +0530

    first commit

---
+++ 21.Chapter 11/06_property_decorator.py
@@ -0,0 +1,12 @@
+class students:
+    s = 1
+
+    @classmethod
+    def show(cls):
+        print(f"The class atribute a is {cls.s}")
+
+e = students()
+e.s = 45
+e.name = "Chandan Naik"
+print(e.name)
+e.show()
