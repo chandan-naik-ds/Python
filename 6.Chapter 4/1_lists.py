77b10951063986c264214dabfd8e679b982d99a7
chandan-naik-ds <chandannnaik03@gmail.com>
2026-03-18 22:13:04 +0530

    first commit

--- 6.Chapter 4/1_lists.py
+++ 6.Chapter 4/1_lists.py
@@ -1,12 +1,12 @@
-# Python lists are the containers to store the values of any data type
+# # Python lists are the containers to store the values of any data type
 
 all = ["Apple","Rohan", 23, 345.77, "Ramesh"]
 
-print(all)
-print(all[1])
-all[1] = "Mahesh"
-print(all[1])
-print(all)  
+# print(all)
+# print(all[1])
+# all[1] = "Mahesh"
+# print(all[1])
+# print(all)  
 # Here the original lisitem can be replaced hence the list are 
 # mutable in python
 # lists can be indexed as a string 
